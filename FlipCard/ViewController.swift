e7882293a5eefd4ae7da80583a595c32f0de121d
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-08-11 22:41:45 +0300

    Added cardButtons Array.

--- FlipCard/ViewController.swift
+++ FlipCard/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var flipsCountLabel: UILabel!
     
+    @IBOutlet var cardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,21 +26,16 @@ class ViewController: UIViewController {
     
     @IBAction func touchButton(_ sender: UIButton) {
         flipsCount += 1
-        flipCard(withEmoji: "🌸", on: sender)
     }
     
-    @IBAction func touchSecondButton(_ sender: UIButton) {
-        flipsCount += 1
-        flipCard(withEmoji: "🌹", on: sender)
-    }
-    
+   
     func flipCard (withEmoji emoji: String, on button: UIButton){
         if button.currentTitle == emoji {
             button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         } else {
             button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
     
