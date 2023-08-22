2579cb88e89c1093d76c4cda845a1492d58d6dfb
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-08-22 23:49:10 +0300

    Fixed bugs.

--- FlipCard/ViewController.swift
+++ FlipCard/ViewController.swift
@@ -26,18 +26,23 @@ class ViewController: UIViewController {
     
     @IBAction func touchButton(_ sender: UIButton) {
         flipsCount += 1
-    }
-    
-   
-    func flipCard (withEmoji emoji: String, on button: UIButton){
-        if button.currentTitle == emoji {
-            button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            print("Card Number: \(cardNumber)")
         } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            print("Chosen card wasn't in cardButtons")
         }
+        
+        
+        func flipCard (withEmoji emoji: String, on button: UIButton){
+            if button.currentTitle == emoji {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            } else {
+                button.setTitle(emoji, for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            }
+        }
+        
+        
     }
-    
-    
 }
