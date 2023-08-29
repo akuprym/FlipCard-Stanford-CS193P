8bf688dc88a83bb447c3141e2a03388687ce6620
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-08-29 20:39:22 +0300

    Created Model: class Concentration & struct Card,

---
+++ FlipCard/Model/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlipCard
+//
+//  Created by admin on 29.08.23.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ FlipCard/Model/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  FlipCard
+//
+//  Created by admin on 29.08.23.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    func chooseCard( at index: Int) {
+        
+    }
+}
