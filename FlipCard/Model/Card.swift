b524e55d8e0d5ab14b7d447532ccae344ee8532c
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-09-08 01:50:33 +0300

    Added static func getUniqueIdentifier() and static var identifierFactory.

--- FlipCard/Model/Card.swift
+++ FlipCard/Model/Card.swift
@@ -13,6 +13,12 @@ struct Card {
     var isMatched = false
     var identifier: Int
     
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
     
     init(identifier: Int) {
         self.identifier = identifier
