49841a542e516893fb24ce1728f10c93106cd6bf
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-09-30 00:00:18 +0300

    added func when cards match.

--- FlipCard/Model/Concentration.swift
+++ FlipCard/Model/Concentration.swift
@@ -10,9 +10,22 @@ import Foundation
 class Concentration {
     
     var cards = [Card]()
+    var indexOfFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
-       
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
