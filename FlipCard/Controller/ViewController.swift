add2d0f205cceb566888ebf3c87bcacccfc38318
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-09-29 23:45:37 +0300

    Fixed bugs. Now emojis are displayed on cards.

--- FlipCard/Controller/ViewController.swift
+++ FlipCard/Controller/ViewController.swift
@@ -35,26 +35,33 @@ class ViewController: UIViewController {
         } else {
             print("Chosen card wasn't in cardButtons")
         }
-        
-        func updateViewFromModel() {
-            for index in cardButtons.indices {
-                let button = cardButtons[index]
-                let card = game.cards[index]
-                if card.isFaceUp {
-                    button.setTitle(emoji(for: card), for: .normal)
-                    button.backgroundColor = .systemIndigo
-                } else {
-                    button.setTitle("", for: .normal)
-                    button.backgroundColor = card.isMatched ? .clear : .systemOrange
-                    
-                }
-            }
-        }
-        var emojiChoices = ["🌸", "🌺", "🌸", "🌺"]
-        
-        func emoji(for card: Card) -> String {
-            return "?"
-        }
     }
     
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = .systemIndigo
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? .clear : .systemOrange
+                
+            }
+        }
+    }
+        var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+        
+        var emoji = [Int:String]()
+        
+        func emoji(for card: Card) -> String {
+            if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+                
+            }
+            return emoji[card.identifier] ?? "0"
+        }
+
 }
--- FlipCard/Model/Concentration.swift
+++ FlipCard/Model/Concentration.swift
@@ -12,11 +12,7 @@ class Concentration {
     var cards = [Card]()
     
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
-        }
+       
     }
     
     init(numberOfPairsOfCards: Int) {
