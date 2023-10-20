b636c9cd7d6a2c8e236c1dbb26bd24e03885d515
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-10-21 00:28:00 +0300

    added computed property - indexOfOneFaceUpCard with get {}

--- FlipCard/Model/Concentration.swift
+++ FlipCard/Model/Concentration.swift
@@ -10,25 +10,45 @@ import Foundation
 class Concentration {
     
     var cards = [Card]()
-    var indexOfFaceUpCard: Int?
+    var indexOfOneFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+            
+        }
+        
+        set {
+            
+        }
+    }
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
-            if let matchIndex = indexOfFaceUpCard, matchIndex != index {
+            if let matchIndex = indexOfOneFaceUpCard, matchIndex != index {
                 // check if cards match
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfFaceUpCard = nil
+                indexOfOneFaceUpCard = nil
             } else {
                 // either no cards or 2 cards are face up
                 for flipDownIndex in cards.indices {
                     cards[flipDownIndex].isFaceUp = false
                 }
                 cards[index].isFaceUp = true
-                indexOfFaceUpCard = index
+                indexOfOneFaceUpCard = index
             }
         }
     }
