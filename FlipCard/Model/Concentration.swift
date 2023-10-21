4f22933967669dc98a22dbc51dab1c956177a400
Anna Kuprym <annakupriyanchik99@gmail.com>
2023-10-21 15:07:45 +0300

    Added *Asserts to protect my API against improper use.

--- FlipCard/Model/Concentration.swift
+++ FlipCard/Model/Concentration.swift
@@ -35,6 +35,7 @@ class Concentration {
     }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneFaceUpCard, matchIndex != index {
                 // check if cards match
@@ -52,6 +53,7 @@ class Concentration {
     }
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
